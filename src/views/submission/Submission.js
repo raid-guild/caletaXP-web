405f27cbc61df3d77dadf455ae3f6a989a7be755
DarrenM <darrenrmills@gmail.com>
2020-06-04 20:32:02 -0700

    submissions page

--- src/views/submission/Submission.js
+++ src/views/submission/Submission.js
@@ -7,11 +7,23 @@ import { get } from '../../utils/Requests';
 import OneUpFeed from '../../components/claims/OneUpFeed';
 import { formattedTime } from '../../utils/Helpers';
 
+import styled from 'styled-components';
+import TelegramIconSrc from '../../assets/img/telegram-icon.svg';
+import DiscordIconSrc from '../../assets/img/discord-icon.svg';
+
+const TelegramIcon = styled.img`
+  width: 22px;
+`;
+const DiscordIcon = styled.img`
+  width: 22px;
+`;
+
 const Submission = ({ match }) => {
   const [loading, setLoading] = useState(false);
   const [oneUps, setOneUps] = useState([]);
   const [submission, setSubmission] = useState([]);
   const [userDetail, setUserDetail] = useState();
+  const [otherUserDetail, setOtherUserDetail] = useState();
   const [user3BoxDetail, setUser3BoxDetail] = useState();
   // const [currentWeb3User, setCurrentUser] = useContext(CurrentUserContext);
 
@@ -75,7 +87,21 @@ const Submission = ({ match }) => {
                 '@' + match.params.username}
             </h2>
             {/* <h3 className="oneup-count">{oneUps.length || 0} 1-Ups</h3> */}
-            {user3BoxDetail && <p>{user3BoxDetail.emoji}</p>}
+            {/* {user3BoxDetail && <p>{user3BoxDetail.emoji}</p>} */}
+            {/* @HELPME Right now there's no way to tell which is which OtherUserDetail, we need logic so the icons are applied correctly */}
+            <div className="other-username-wrapper">
+              <h3>
+                <TelegramIcon src={TelegramIconSrc} />
+                {(userDetail && userDetail.username) ||
+                  '@' + match.params.username}
+              </h3>
+
+              {otherUserDetail ? (
+                <h3>
+                  <DiscordIcon src={DiscordIconSrc} />
+                  {otherUserDetail.username}</h3>
+              ) : null}
+            </div>
           </Col>
           <Col>
             {submission.fields ? (
