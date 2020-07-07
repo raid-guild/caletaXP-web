94e29ebcec4ca761d545a29392e4b072f226fa0a
Dekan <dekanbro@gmail.com>
2020-07-07 16:00:28 -0600

    update xdai up token addr

--- src/contexts/Store.js
+++ src/contexts/Store.js
@@ -16,7 +16,7 @@ export const addresses = {
     upToken: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
   },
   xdai: {
-    upToken: '0x385a78159a02128439cafcfdb7797ede99bf4a5f',
+    upToken: '0x45795690bfdB896Fde04CE318C3bB853AE5baf2A',
   },
 };
 
